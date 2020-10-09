56f1082823b974a1315bafa73095a92009e28794
unknown <e2010101@edu.cc.uec.ac.jp>
2020-10-09 19:02:28 +0900

    ページ１を変更

--- src/Page1/index.js
+++ src/Page1/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function() {
   return (
     <>
-      <h1 className="text-2xl">Page1</h1>
+      <h1 className="text-2xl">京王線</h1>
       <div className="p-4">
-        <p>Hello World</p>
+        <p>なぜ京王線と京王新線があるのでしょうか。迷いました。</p>
       </div>
     </>
   )
